c1bc3b9347d1e5b753e81e137ee297f4baaab8c6
Justinas Teresius <justinast@gmail.com>
2020-02-29 22:25:09 +0000

    Creating archivedShows model and endpoint

--- models/index.js
+++ models/index.js
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose';
 import Show from './show';
+import ArchivedShow from './archivedShow';
 
 const connectDb = () => mongoose.connect(process.env.DATABASE_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
-const models = { Show };
+const models = { Show, ArchivedShow };
 
 export { connectDb };
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
+import mongoose from 'mongoose';
+
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+/* GET shows. */
+router.get('/stored-shows', async function(req, res, next) {
+  const shows = await mongoose.model('Show').find({});
+
+  return res.send(shows);
+});
+
+/* GET archived shows. */
+router.get('/archived-shows', async function(req, res, next) {
+  const shows = await mongoose.model('ArchivedShow').find({});
+  console.log(shows);
+
+  return res.send(shows);
 });
 
 module.exports = router;
