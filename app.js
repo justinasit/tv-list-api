f05b732c5fb213b46a837d23df123816c379fd45
Justinas Teresius <justinast@gmail.com>
2020-02-29 22:24:48 +0000

    Removing shows file from app.js

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var showsRouter = require('./routes/shows');
 
 var app = express();
 
@@ -23,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/shows', showsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
