6a23b141e22759b03c4d09c75858650445717627
Justinas Teresius <justinast@gmail.com>
2020-03-07 15:58:51 +0000

    Clean up

--- models/index.js
+++ models/index.js
@@ -1,6 +1,4 @@
 import mongoose from 'mongoose';
-import Show from './show';
-import ArchivedShow from './archivedShow';
 import User from './user';
 
 const connectDb = () => mongoose.connect(process.env.DATABASE_URL, {
@@ -8,7 +6,7 @@ const connectDb = () => mongoose.connect(process.env.DATABASE_URL, {
     useUnifiedTopology: true
 });
 
-const models = { Show, ArchivedShow, User };
+const models = { User };
 
 export { connectDb };
 
