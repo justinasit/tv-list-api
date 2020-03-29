3c060c722c02b3e85f7a22d9ad5773c38f73559c
Justinas Teresius <justinast@gmail.com>
2020-03-29 15:35:12 +0100

    Refreshing db function

--- setupJest.js
+++ setupJest.js
@@ -1,3 +1,10 @@
 import 'regenerator-runtime/runtime';
+import mongoose from 'mongoose';
 
 process.env.DATABASE_URL = 'mongodb://127.0.0.1/shows_test';
+
+const refreshDB = async () => {
+    await mongoose.model('User').deleteMany({});
+}
+
+export default refreshDB;
