df518a91a3b78c1bf693faeb46d776354fb83e79
Justinas Teresius <justinast@gmail.com>
2020-04-20 13:27:31 +0100

    Exposing x-auth-token in cors

--- app.js
+++ app.js
@@ -15,7 +15,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(cors());
+app.use(cors({
+  'exposedHeaders': 'x-auth-token',
+}));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
